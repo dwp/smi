20344cdca65dbc192deec024fcad4486da615e76
Simon Wilson <simoncwilson@gmail.com>
2017-11-28 21:57:50 +0000

    End of day push for  design v4

--- app/routes.js
+++ app/routes.js
@@ -27,11 +27,63 @@ router.get('/smi/v*/dosearch/',function(req,res,next)
 
 });
 
+
+
 router.get('/smi/v*/person*', function (req, res, next) {
   req.data = req.data || { };
   req.data.nino = req.query.nino;
   next();
 })
+
+router.get('/smi/v*/search-nino/',function(req,res,next)
+{
+  var v = req.params[0];
+  req.data = req.data || { };
+  req.data.nino = req.query.nino;
+
+  const nino = req.data.nino;
+
+  if (req.query.nino == '') {
+    res.redirect('/smi/v'+v+"/search-nino-no-entry")
+  }
+  else if (req.query.nino == 'GB654321C' || req.query.nino == 'ZZ918273C') {
+    res.redirect('/smi/v'+v+"/search-nino-found?nino="+nino)
+  }
+  else {
+    res.redirect('/smi/v'+v+"/search-nino-none?nino="+nino)
+  }
+
+});
+
+router.get('/smi/v*/search-nino-*/',function(req,res,next)
+{
+  var v = req.params[0];
+  req.data = req.data || { };
+  req.data.nino = req.query.nino;
+  const nino = req.data.nino;
+  next();
+});
+
+
+router.get('/smi/v*/account-details/',function(req,res,next)
+{
+  var v = req.params[0];
+  req.data = req.data || { };
+
+  const nino = req.data.nino;
+
+  if (req.query.payingTo == "lender") {
+    res.redirect('/smi/v'+v+"/account-details-lender")
+  }
+  else if (req.query.payingTo == "applicant") {
+    res.redirect('/smi/v'+v+"/account-details-applicant")
+  }
+  else {
+    res.redirect('/smi/v'+v+"/account-details-none")
+  }
+
+});
+
 
 /*
   Redirect all posts to gets.
