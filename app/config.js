2ae35ae802882b2eaa036d21dc155765aace4cb2
Gary Boyle <gary.boyle@dwp.gsi.gov.uk>
2019-02-20 12:30:47 +0000

    Updated all the way to 8.7.0

--- app/config.js
+++ app/config.js
@@ -12,15 +12,23 @@ module.exports = {
   // Enable or disable password protection on production
   useAuth: 'true',
 
+  // Automatically stores form data, and send to all views
+  useAutoStoreData: 'true',
+
+  // Enable cookie-based session store (persists on restart)
+  // Please note 4KB cookie limit per domain, cookies too large will silently be ignored
+  useCookieSessionStore: 'false',
+
   // Enable or disable built-in docs and examples.
   useDocumentation: 'true',
 
-  // Force HTTP to redirect to HTTPs on production
+  // Force HTTP to redirect to HTTPS on production
   useHttps: 'true',
 
-  useBrowserSync: 'true',
-
   // Cookie warning - update link to service's cookie page.
-  cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#">Find out more about cookies</a>'
+  cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#">Find out more about cookies</a>',
+
+  // Enable or disable Browser Sync
+  useBrowserSync: 'true'
 
 }
